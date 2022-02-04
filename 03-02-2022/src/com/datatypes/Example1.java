678c4956af66d70b296ac57815de3ad0957aeec4
PAVANI B <PAVANI B@LAPTOP-7VMGP23T>
2022-02-04 12:59:08 +0530

    second commit

---
+++ 03-02-2022/src/com/datatypes/Example1.java
@@ -0,0 +1,18 @@
+package com.datatypes;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		/**
+		 * Write a java program to print customer information(id,name and address)
+		 */
+		int CustomerId=7886;
+		String Customername="pavani";
+		String CustomerAddress="Hyderabad";
+		System.out.println(CustomerId);
+		System.out.println(Customername);
+		System.out.println(CustomerAddress);
+
+	}
+
+}
